1fe09ec4b9e18aac0085704020ec008909c19915
Laurent CLAISSE <laurent_claisse@hotmail.fr>
2014-06-26 19:41:40 +0200

    aa

---
+++ code/command/src/test/java/cla/command/CommandTest.java
@@ -0,0 +1,16 @@
+package cla.command;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class CommandTest {
+
+	@Test public void plus() {
+//		assertEquals(
+//				3D, 
+//				PLUS.apply(1D, 2D),
+//				0D
+//		);
+	}
+	
+}
