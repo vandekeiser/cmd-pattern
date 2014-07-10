7c2222cc3694442347037a804ac928758e586cc7
Laurent CLAISSE <laurent_claisse@hotmail.fr>
2014-07-10 20:33:06 +0200

    compensation conversation sym memento

--- code/command/src/test/java/cla/command/undo/compensation/CommandUndoTest_Compensation_Typing.java
+++ code/command/src/test/java/cla/command/undo/compensation/CommandUndoTest_Compensation_Typing.java
@@ -26,9 +26,27 @@ public class CommandUndoTest_Compensation_Typing extends BaseCommandTest {
 	/**
 	 * a    --> "a" 
 	 * undo --> ""
+	 * undo --> ""
+	 */
+	@Test public void undo_undo() {
+		CompensationConversation commands = new CompensationConversation(env);
+		
+		commands.exec(CommandFactory.typeString("a"));
+		assertEquals("a", env.display().displayed());
+		
+		commands.undo();
+		assertEquals("", env.display().displayed());
+		
+		commands.undo();
+		assertEquals("", env.display().displayed());
+	}
+	
+	/**
+	 * a    --> "a" 
+	 * undo --> ""
 	 * redo --> "a"
 	 */
-	@Test public void redo() {
+	@Test public void undo_redo() {
 		CompensationConversation commands = new CompensationConversation(env);
 		
 		commands.exec(CommandFactory.typeString("a"));
@@ -47,7 +65,7 @@ public class CommandUndoTest_Compensation_Typing extends BaseCommandTest {
 	 * redo --> "a"
 	 * undo --> ""
 	 */
-	@Test public void redo_undo() {
+	@Test public void undo_redo_undo() {
 		CompensationConversation commands = new CompensationConversation(env);
 		
 		commands.exec(CommandFactory.typeString("a"));
@@ -58,9 +76,6 @@ public class CommandUndoTest_Compensation_Typing extends BaseCommandTest {
 		
 		commands.redo();
 		assertEquals("a", env.display().displayed());
-		
-		commands.undo();
-		assertEquals("", env.display().displayed());
 	}
 	
 
@@ -69,8 +84,156 @@ public class CommandUndoTest_Compensation_Typing extends BaseCommandTest {
 	 */
 	@Test public void nothingToUndo_Noop() {
 		CompensationConversation commands = new CompensationConversation(env);
+		
 		commands.undo();
-		assertEquals(0, env.carrots().numberOfCarrots());
+		assertEquals("", env.display().displayed());
+	}
+	/**
+	 * undo --> ""
+	 * undo --> ""
+	 */
+	@Test public void nothingToUndoUndo_Noop() {
+		CompensationConversation commands = new CompensationConversation(env);
+		
+		commands.undo();
+		assertEquals("", env.display().displayed());
+		
+		commands.undo();
+		assertEquals("", env.display().displayed());
 	}
 	
+	/**
+	 * redo --> ""
+	 */
+	@Test public void nothingToRedo_Noop() {
+		CompensationConversation commands = new CompensationConversation(env);
+		
+		commands.redo();
+		assertEquals("", env.display().displayed());
+	}
+	/**
+	 * redo --> ""
+	 * redo --> ""
+	 */
+	@Test public void nothingToRedoRedo_Noop() {
+		CompensationConversation commands = new CompensationConversation(env);
+		
+		commands.redo();
+		assertEquals("", env.display().displayed());
+		
+		commands.redo();
+		assertEquals("", env.display().displayed());
+	}
+
+	/**
+	 * a    --> "a"
+	 * b    --> "b" 
+	 * undo --> ""
+	 */
+	@Test public void typeA_typeB_undo_undo() {
+		CompensationConversation commands = new CompensationConversation(env);
+		
+		System.out.println();System.out.println();
+		commands.exec(CommandFactory.typeString("a"));
+		assertEquals("a", env.display().displayed());
+		
+		System.out.println();System.out.println();
+		commands.exec(CommandFactory.typeString("b"));
+		assertEquals("ab", env.display().displayed());
+		
+		System.out.println();System.out.println();
+		commands.undo();
+		assertEquals("a", env.display().displayed());
+		
+		System.out.println();System.out.println();
+		commands.undo();
+		assertEquals("", env.display().displayed());
+	}
+	
+	@Test public void complexConversation() {
+		CompensationConversation commands = new CompensationConversation(env);
+		
+		commands.exec(CommandFactory.typeString("a"));
+		assertEquals("a", env.display().displayed());
+		
+		System.out.println();System.out.println();
+		commands.undo();
+		assertEquals("", env.display().displayed());
+		
+		System.out.println();System.out.println();
+		commands.exec(CommandFactory.typeString("b"));
+		assertEquals("b", env.display().displayed());
+		
+		System.out.println();System.out.println();
+		commands.exec(CommandFactory.typeString("c"));
+		assertEquals("bc", env.display().displayed());
+		
+		System.out.println();System.out.println();
+		commands.exec(CommandFactory.typeString("d"));
+		assertEquals("bcd", env.display().displayed());
+		
+		System.out.println();System.out.println();
+		commands.undo();
+		assertEquals("bc", env.display().displayed());
+		
+		System.out.println();System.out.println();
+		commands.redo();
+		assertEquals("bcd", env.display().displayed());
+		
+		System.out.println();System.out.println();
+		commands.undo();
+		assertEquals("bc", env.display().displayed());
+		
+		System.out.println();System.out.println();
+		commands.undo();
+		assertEquals("b", env.display().displayed());
+		
+		System.out.println();System.out.println();
+		commands.exec(CommandFactory.typeString("e"));
+		assertEquals("be", env.display().displayed());
+		
+		System.out.println();System.out.println();
+		commands.exec(CommandFactory.typeString("f"));
+		assertEquals("bef", env.display().displayed());
+		
+		System.out.println();System.out.println();
+		commands.undo();
+		assertEquals("be", env.display().displayed());
+		
+		System.out.println();System.out.println();
+		commands.undo();
+		assertEquals("b", env.display().displayed());
+		
+		System.out.println();System.out.println();
+		commands.undo();
+		assertEquals("", env.display().displayed());
+		
+		System.out.println();System.out.println();
+		commands.redo();
+		assertEquals("b", env.display().displayed());
+		
+		System.out.println();System.out.println();
+		commands.redo();
+		assertEquals("be", env.display().displayed());
+		
+		System.out.println();System.out.println();
+		commands.redo();
+		assertEquals("bef", env.display().displayed());
+		
+		System.out.println();System.out.println();
+		commands.redo();
+		assertEquals("bef", env.display().displayed());
+		
+		System.out.println();System.out.println();
+		commands.undo();
+		assertEquals("be", env.display().displayed());
+		
+		System.out.println();System.out.println();
+		commands.undo();
+		assertEquals("b", env.display().displayed());
+		
+		System.out.println();System.out.println();
+		commands.undo();
+		assertEquals("", env.display().displayed());
+	}
 }
