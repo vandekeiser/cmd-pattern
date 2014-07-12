7720734d7ca12bb425ce7b6804ca670f38b1932a
Laurent CLAISSE <laurent_claisse@hotmail.fr>
2014-07-12 18:00:15 +0200

    recommence ReplayConversation

--- code/command/src/main/java/cla/command/undo/replay/ReplayConversation.java
+++ code/command/src/main/java/cla/command/undo/replay/ReplayConversation.java
@@ -7,58 +7,40 @@ import cla.domain.Env;
 public class ReplayConversation implements Conversation<ResetableCommand> {
 
 	private final Env env;
-	private ReplayableList doneList, undoList, redoList;
+	private ReplayableList undoList, redoList;
 	
 	public ReplayConversation(Env env) {
 		this.env = env;
-		this.doneList = new ReplayableList();
 		this.undoList = new ReplayableList();
 		this.redoList = new ReplayableList();
 	}
 
 	@Override public void exec(ResetableCommand todo) {
-		System.out.println("ReplayConversation/exec/START/doneList: " + doneList);
 		System.out.println("ReplayConversation/exec/START/undoList: " + undoList);
 		System.out.println("ReplayConversation/exec/START/redoList: " + redoList);
 		
-		undoList = doneList.copy();
 		todo.execute(this.env);
-		doneList.addLast(todo);
-		redoList = doneList.copy();
 		
-		System.out.println("ReplayConversation/exec/END/doneList: " + doneList);
 		System.out.println("ReplayConversation/exec/END/undoList: " + undoList);
 		System.out.println("ReplayConversation/exec/END/redoList: " + redoList);
 	}
 
 	@Override public void undo() {
-		System.out.println("ReplayConversation/undo/START/doneList: " + doneList);//ReplayableList{resetables: [TypeString{stringToType:a}]}
-		System.out.println("ReplayConversation/undo/START/undoList: " + undoList);//ReplayableList{resetables: []}
-		System.out.println("ReplayConversation/undo/START/redoList: " + redoList);//ReplayableList{resetables: [TypeString{stringToType:a}]}
-		
-		redoList = doneList.copy();
-		doneList.reset(env);
+		System.out.println("ReplayConversation/undo/START/undoList: " + undoList);
+		System.out.println("ReplayConversation/undo/START/redoList: " + redoList);
+
 		undoList.replay(env);
-		doneList = undoList.copy();
-		undoList.removeLast();
 		
-		System.out.println("ReplayConversation/undo/END/doneList: " + doneList);
 		System.out.println("ReplayConversation/undo/END/undoList: " + undoList);
 		System.out.println("ReplayConversation/undo/END/redoList: " + redoList);
 	}
 
 	@Override public void redo() {
-		System.out.println("ReplayConversation/redo/START/doneList: " + doneList);
 		System.out.println("ReplayConversation/redo/START/undoList: " + undoList);
 		System.out.println("ReplayConversation/redo/START/redoList: " + redoList);
-		
-		undoList = doneList.copy();
-		doneList.reset(env);
+
 		redoList.replay(env);
-		doneList = redoList.copy();
-		redoList.removeLast();
 		
-		System.out.println("ReplayConversation/redo/END/doneList: " + doneList);
 		System.out.println("ReplayConversation/redo/END/undoList: " + undoList);
 		System.out.println("ReplayConversation/redo/END/redoList: " + redoList);
 	}
--- code/command/src/main/java/cla/command/undo/replay/ReplayableList.java
+++ code/command/src/main/java/cla/command/undo/replay/ReplayableList.java
@@ -2,54 +2,66 @@ package cla.command.undo.replay;
 
 import java.util.LinkedList;
 
+import cla.command.Command;
 import cla.domain.Env;
 
 public class ReplayableList {
 
-	private final LinkedList<ResetableCommand> resetables;
+	private final LinkedList<Command> resets;
+	private final LinkedList<Command> replays;
 
 	ReplayableList() {
-		this(new LinkedList<>());
-	}
-	
-	ReplayableList(LinkedList<ResetableCommand> resetables) {
-		this.resetables = new LinkedList<>(resetables);//defensive copy
-	}
-
-	public void addLast(ResetableCommand cmd) {
-		resetables.add(cmd);
-	}
-
-	public void clear() {
-		resetables.clear();		
-	}
-
-	public ResetableCommand peekLast() {
-		return resetables.peekLast();
-	}
-	
-	public ResetableCommand removeLast() {
-		return resetables.pollLast();
+		this.resets = new LinkedList<>();
+		this.replays = new LinkedList<>();
 	}
 
 	public void reset(Env env) {
-		resetables.stream().forEach(cmd->{
-			cmd.resetCmd().execute(env);
-		});
-	}
-	
-	public void replay(Env env) {
-		resetables.stream().forEachOrdered(cmd->{
+		resets.stream().forEach(cmd->{
 			cmd.execute(env);
 		});
 	}
 	
-	@Override public String toString() {
-		return String.format("%s{resetables: %s}", ReplayableList.class.getSimpleName(), resetables);
+	public void replay(Env env) {
+		replays.stream().forEachOrdered(cmd->{
+			cmd.execute(env);
+		});		
 	}
 
-	public ReplayableList copy() {
-		return new ReplayableList(this.resetables);
-	}
+	
+//	public void addLast(ResetableCommand cmd) {
+//		resetables.add(cmd);
+//	}
+//
+//	public void clear() {
+//		resetables.clear();		
+//	}
+//
+//	public ResetableCommand peekLast() {
+//		return resetables.peekLast();
+//	}
+//	
+//	public ResetableCommand removeLast() {
+//		return resetables.pollLast();
+//	}
+//
+//	public void reset(Env env) {
+//		resetables.stream().forEach(cmd->{
+//			cmd.resetCmd().execute(env);
+//		});
+//	}
+//	
+//	public void replay(Env env) {
+//		resetables.stream().forEachOrdered(cmd->{
+//			cmd.execute(env);
+//		});
+//	}
+//	
+//	@Override public String toString() {
+//		return String.format("%s{resetables: %s}", ReplayableList.class.getSimpleName(), resetables);
+//	}
+//
+//	public ReplayableList copy() {
+//		return new ReplayableList(this.resetables);
+//	}
 
 }
