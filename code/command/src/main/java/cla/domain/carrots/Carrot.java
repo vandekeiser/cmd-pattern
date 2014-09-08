d7fdb16b0bc24f7f8eb857cc1be3e559ed10ecf7
Laurent CLAISSE <laurent_claisse@hotmail.fr>
2014-09-08 21:02:17 +0200

    rm carrots

--- code/command/src/main/java/cla/domain/carrots/Carrot.java
+++
@@ -1,9 +0,0 @@
-package cla.domain.carrots;
-
-public class Carrot {
-
-	@Override public String toString() {
-		return String.format("%s", Carrot.class.getSimpleName());
-	}
-	
-}
--- code/command/src/test/java/cla/command/BaseCommandTest.java
+++ code/command/src/test/java/cla/command/BaseCommandTest.java
@@ -5,21 +5,16 @@ import static org.junit.Assert.assertEquals;
 import org.junit.After;
 import org.junit.Before;
 
-import cla.domain.carrots.CarrotRepository;
-import cla.domain.carrots.TestCarrotRepository;
 import cla.domain.typing.Display;
 import cla.domain.typing.TestDisplay;
 
 public abstract class BaseCommandTest {
 
-	protected CarrotRepository repo;
 	protected Display display;
 	
 	//--------setup/teardown VVVVVVVVV
 	@Before public void setup() {
 		display = new TestDisplay();
-		repo = new TestCarrotRepository();
-		assertEquals(0, repo.numberOfCarrots());//sanity check
 		assertEquals("", display.displayed());//sanity check
 	}
 	
