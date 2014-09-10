6bc7c54cbc2eb270afebc5b639a731170ca9122b
Laurent CLAISSE <laurent_claisse@hotmail.fr>
2014-09-10 07:29:32 +0200

    noms et commentaires des tests

--- code/command/src/test/java/cla/command/SimpleCommandTest_Typing_Test.java
+++ code/command/src/test/java/cla/command/SimpleCommandTest_Typing_Test.java
@@ -8,6 +8,10 @@ import cla.domain.typing.TypeString;
 
 public class SimpleCommandTest_Typing_Test extends BaseCommandTest {
 
+	
+	/**
+	 * Sanity check 
+	 */
 	@Test public void basicCommandExecution_typeString() {
 		Command typeString = new TypeString(display, "a"); 
 		typeString.execute();
--- code/command/src/test/java/cla/command/undo/CommandUndoTest_Typing.java
+++ code/command/src/test/java/cla/command/undo/CommandUndoTest_Typing.java
@@ -2,7 +2,6 @@ package cla.command.undo;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import cla.command.BaseCommandTest;
@@ -14,10 +13,11 @@ public abstract class CommandUndoTest_Typing<C extends Command> extends BaseComm
 	protected abstract C typeString(String stringToType);
 	
 	/**
+	 * Basic undo
 	 * a    --> "a" 
 	 * undo --> ""
 	 */
-	@Test public void undo() {
+	@Test public void basicUndo() {
 		Conversation<C> commands = newConversation();
 		
 		commands.exec(typeString("a"));
@@ -28,11 +28,38 @@ public abstract class CommandUndoTest_Typing<C extends Command> extends BaseComm
 	}
 	
 	/**
+	 * Undo is noop when there were no execs
+	 * undo --> ""
+	 */
+	@Test public void basicNoopUndo() {
+		Conversation<C> commands = newConversation();
+		
+		commands.undo();
+		assertEquals("", display.displayed());
+	}
+	
+	/**
+	 * Undo is noop when there were no execs, even when undo is called several times 
+	 * undo --> ""
+	 * undo --> ""
+	 */
+	@Test public void basicNoopUndoTwice() {
+		Conversation<C> commands = newConversation();
+		
+		commands.undo();
+		assertEquals("", display.displayed());
+		
+		commands.undo();
+		assertEquals("", display.displayed());
+	}
+	
+	/**
+	 * Undo is noop when there is nothing more to undo
 	 * a    --> "a" 
 	 * undo --> ""
 	 * undo --> ""
 	 */
-	@Test public void undo_undo() {
+	@Test public void noopUndo() {
 		Conversation<C> commands = newConversation();
 		
 		commands.exec(typeString("a"));
@@ -46,11 +73,12 @@ public abstract class CommandUndoTest_Typing<C extends Command> extends BaseComm
 	}
 	
 	/**
+	 * Basic redo
 	 * a    --> "a" 
 	 * undo --> ""
 	 * redo --> "a"
 	 */
-	@Test public void undo_redo() {
+	@Test public void basicRedo() {
 		Conversation<C> commands = newConversation();
 		
 		commands.exec(typeString("a"));
@@ -64,12 +92,39 @@ public abstract class CommandUndoTest_Typing<C extends Command> extends BaseComm
 	}
 	
 	/**
+	 * Redo is noop when there were no undos
+	 * redo --> ""
+	 */
+	@Test public void basicNoopRedo() {
+		Conversation<C> commands = newConversation();
+		
+		commands.redo();
+		assertEquals("", display.displayed());
+	}
+	
+	/**
+	 * Redo is noop when there were no undos, even when redo is called several times 
+	 * redo --> ""
+	 * redo --> ""
+	 */
+	@Test public void basicNoopRedoTwice() {
+		Conversation<C> commands = newConversation();
+		
+		commands.redo();
+		assertEquals("", display.displayed());
+		
+		commands.redo();
+		assertEquals("", display.displayed());
+	}
+	
+	/**
+	 * Slightly more complex interaction between undo and redo
 	 * a    --> "a" 
 	 * undo --> ""
 	 * redo --> "a"
 	 * undo --> ""
 	 */
-	@Test public void undo_redo_undo() {
+	@Test public void exec_undo_redo_undo() {
 		Conversation<C> commands = newConversation();
 		
 		commands.exec(typeString("a"));
@@ -80,58 +135,15 @@ public abstract class CommandUndoTest_Typing<C extends Command> extends BaseComm
 		
 		commands.redo();
 		assertEquals("a", display.displayed());
-	}
-	
-
-	/**
-	 * undo --> ""
-	 */
-	@Test public void nothingToUndo_Noop() {
-		Conversation<C> commands = newConversation();
-		
-		commands.undo();
-		assertEquals("", display.displayed());
-	}
-	/**
-	 * undo --> ""
-	 * undo --> ""
-	 */
-	@Test public void nothingToUndoUndo_Noop() {
-		Conversation<C> commands = newConversation();
-		
-		commands.undo();
-		assertEquals("", display.displayed());
 		
 		commands.undo();
 		assertEquals("", display.displayed());
 	}
 	
-	/**
-	 * redo --> ""
-	 */
-	@Test public void nothingToRedo_Noop() {
-		Conversation<C> commands = newConversation();
-		
-		commands.redo();
-		assertEquals("", display.displayed());
-	}
-	/**
-	 * redo --> ""
-	 * redo --> ""
-	 */
-	@Test public void nothingToRedoRedo_Noop() {
-		Conversation<C> commands = newConversation();
-		
-		commands.redo();
-		assertEquals("", display.displayed());
-		
-		commands.redo();
-		assertEquals("", display.displayed());
-	}
-
 	/**
 	 * a    --> "a"
-	 * b    --> "b" 
+	 * b    --> "ab" 
+	 * undo --> "a"
 	 * undo --> ""
 	 */
 	@Test public void typeA_typeB_undo_undo() {
@@ -148,6 +160,58 @@ public abstract class CommandUndoTest_Typing<C extends Command> extends BaseComm
 		
 		commands.undo();
 		assertEquals("", display.displayed());
+	}
+	
+	/**
+	 * a    --> "a"
+	 * b    --> "ab" 
+	 * undo --> "a"
+	 * redo --> "ab"
+	 */
+	@Test public void exec_exec_undo_redo() {
+		Conversation<C> commands = newConversation();
+		
+		commands.exec(typeString("a"));
+		assertEquals("a", display.displayed());
+		
+		commands.exec(typeString("b"));
+		assertEquals("ab", display.displayed());
+		
+		commands.undo();
+		assertEquals("a", display.displayed());
+		
+		commands.redo();
+		assertEquals("ab", display.displayed());
+	}
+	
+	/**
+	 * a    --> "a"
+	 * b    --> "ab" 
+	 * undo --> "a"
+	 * undo --> ""
+	 * redo --> "a"
+	 * redo --> "ab"
+	 */
+	@Test public void exec_exec_undo_undo_redo_redo() {
+		Conversation<C> commands = newConversation();
+		
+		commands.exec(typeString("a"));
+		assertEquals("a", display.displayed());
+		
+		commands.exec(typeString("b"));
+		assertEquals("ab", display.displayed());
+		
+		commands.undo();
+		assertEquals("a", display.displayed());
+		
+		commands.undo();
+		assertEquals("", display.displayed());
+		
+		commands.redo();
+		assertEquals("a", display.displayed());
+		
+		commands.redo();
+		assertEquals("ab", display.displayed());
 	}
 	
 	@Test public void complexConversation() {
@@ -217,65 +281,4 @@ public abstract class CommandUndoTest_Typing<C extends Command> extends BaseComm
 		assertEquals("", display.displayed());
 	}
 	
-	@Test public void exec_exec_undo_redo() {
-		Conversation<C> commands = newConversation();
-		
-		commands.exec(typeString("a"));
-		assertEquals("a", display.displayed());
-		
-		commands.exec(typeString("b"));
-		assertEquals("ab", display.displayed());
-		
-		commands.undo();
-		assertEquals("a", display.displayed());
-		
-		commands.redo();
-		assertEquals("ab", display.displayed());
-	}
-	
-	@Test public void exec_exec_undo_undo() {
-		Conversation<C> commands = newConversation();
-		
-		commands.exec(typeString("a"));
-		assertEquals("a", display.displayed());
-		
-		commands.exec(typeString("b"));
-		assertEquals("ab", display.displayed());
-		
-		commands.undo();
-		assertEquals("a", display.displayed());
-		
-		commands.undo();
-		assertEquals("", display.displayed());
-	}
-	
-	@Test public void exec_exec_undo_undo_redo_redo() {
-		Conversation<C> commands = newConversation();
-		
-		commands.exec(typeString("a"));
-		assertEquals("a", display.displayed());
-		
-		commands.exec(typeString("b"));
-		assertEquals("ab", display.displayed());
-		
-		commands.undo();
-		assertEquals("a", display.displayed());
-		
-		commands.undo();
-		assertEquals("", display.displayed());
-		
-		commands.redo();
-		assertEquals("a", display.displayed());
-		
-		commands.redo();
-		assertEquals("ab", display.displayed());
-	}
-	
-	@Test @Ignore public void failureCase() {
-		//TODO
-	}
-	
-	@Test @Ignore public void undoAvec2TypesDeResets() {
-		//TODO
-	}
 }
